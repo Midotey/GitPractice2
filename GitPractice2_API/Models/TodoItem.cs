f15db561ae818d8966e95e833cc7690049db3ef8
Midotey <shin.alexey2@gmail.com>
2024-04-25 19:00:15 +0300

    Start

---
+++ GitPractice2_API/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace GitPractice2_API.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Task { get; set; }
+        public bool IsComplete { get; set; }
+        public TodoItem() { }
+        public TodoItem(string name, string task)
+        {
+            Name = name;
+            Task = task;
+        }
+    }
+}
---
+++ GitPractice2_API/TodoItemContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GitPractice2_API
+{
+    public class TodoItemContext : DbContext
+    {
+        public TodoItemContext : base(){
+
+    }
+}
