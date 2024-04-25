f1f3b6b4de75747cdf9ba54e84fc5e3f160ff26a
Midotey <shin.alexey2@gmail.com>
2024-04-25 19:00:15 +0300

    Start

---
+++ GitPractice2_API/TodoRepository.cs
@@ -0,0 +1,22 @@
+﻿using GitPractice2_API.Models;
+
+namespace GitPractice2_API
+{
+    public interface ITodoRepository : IDisposable
+    {
+        IEnumerable<TodoItem> GetTodoItems();
+        TodoItem GetTodoItem(int id);
+        void CreateTodoItem(TodoItem todoItem);
+        void DeleteTodoItem(int id);
+        void UpdateTodoItem(int id);
+    }
+    public class TodoRepository : ITodoRepository
+    {
+        private readonly ITodoRepository rep;
+        public TodoRepository(ITodoRepository rep)
+        {
+            this.rep = rep;
+        }
+
+    }
+}
