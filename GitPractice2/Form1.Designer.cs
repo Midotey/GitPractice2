436a7bfebe503301ed12745277f60d05ca7ef2b9
Midotey <shin.alexey2@gmail.com>
2024-05-02 18:17:16 +0300

    Little bit corrected api, and created winform. All done!

--- GitPractice2/Form1.Designer.cs
+++ GitPractice2/Form1.Designer.cs
@@ -28,19 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            comboBox1 = new ComboBox();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 41);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(630, 376);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(648, 354);
+            button2.Name = "button2";
+            button2.Size = new Size(140, 63);
+            button2.TabIndex = 3;
+            button2.Text = "Add new note";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(648, 285);
+            button3.Name = "button3";
+            button3.Size = new Size(140, 63);
+            button3.TabIndex = 4;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(630, 23);
+            comboBox1.TabIndex = 6;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 419);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(85, 19);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Completed";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(648, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 62);
+            button1.TabIndex = 8;
+            button1.Text = "Edit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private ComboBox comboBox1;
+        private CheckBox checkBox1;
+        private Button button1;
     }
 }
